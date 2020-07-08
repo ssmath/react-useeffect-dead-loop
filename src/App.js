0d6e7cb8eeb92f9fed5e9ab1e17b34e8fb773788
ssmath <164354491@qq.com>
2020-07-08 21:43:17 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import request from 'umi-request';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    request('https://jsonplaceholder.typicode.com/todos/', {
+     method: 'get',
+    })
+    .then(response => {
+      console.log('fetch data');
+      setData(response);
+    })
+    .catch(error => {
+      console.log("report error: ", error);
+    })
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          {data.map(item => {
+            return (
+            <li key={item.id}>{item.title}</li>
+            );
+          })}
+        </ul>
       </header>
     </div>
   );
